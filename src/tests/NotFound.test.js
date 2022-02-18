2976e27b8fde5a64a9c7f5fecfe12433b321f31f
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-02-18 15:44:11 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import NotFound from '../components/NotFound';
+
+describe('Teste a página contém as informações', () => {
+  test('contém um heading h2 com o texto Page requested not found 😭', () => {
+    render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+    const title = screen.getByRole('heading', {
+      name: 'Page requested not found Crying emoji',
+    });
+    expect(title).toBeInTheDocument();
+  });
+
+  test('a img com a url requerida.', () => {
+    render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+    const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const img = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(img.src).toBe(URL);
+  });
+});
