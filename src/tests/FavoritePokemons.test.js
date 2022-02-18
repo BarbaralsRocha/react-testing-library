e97d7f31b6926c629426f1e5d00d1ae9050aaf15
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-02-18 15:30:45 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('Teste a página contém as informações', () => {
+  test('mensagem No favorite pokemon found, se não tiver pokémons favoritos.', () => {
+    render(
+      <MemoryRouter>
+        <FavoritePokemons />
+      </MemoryRouter>,
+    );
+    const notFoundMessage = screen.getByText('No favorite pokemon found');
+    expect(notFoundMessage).toBeInTheDocument();
+  });
+
+  test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    render(
+      <MemoryRouter>
+        <FavoritePokemons pokemons={ pokemons } />
+      </MemoryRouter>,
+    );
+    const pikachu = screen.getByText('Pikachu');
+    expect(pikachu).toBeInTheDocument();
+  });
+});
